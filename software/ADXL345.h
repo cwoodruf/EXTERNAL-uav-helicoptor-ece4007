aab0b1a014e3d2efc8992a2a865bf556cc451267
joey <joey@jyore.com>
2012-01-15 13:56:18 -0500

    Updated Get/Set Methods for ADXL345

--- software/ADXL345.h
+++ software/ADXL345.h
@@ -5,68 +5,73 @@
 #include "util.h"
 #include <stdlib.h>
 
+
+//TODO: Add SubFunctions for setting the bits in registers
+
 class ADXL345 {
 
 	private:
 		I2C recvLine;
 		I2C sendLine;
 
+		unsigned char getByte(char addr);
+		void sendByte(char addr, char value);
+
 	public:
 		ADXL345(unsigned char channel);
 		ADXL345(unsigned char channel,bool alt);
 
-		
 
 		//Read Methods
-		int getDevId();
-		int getTapThreshold();
-		int getXaxisOffset();
-		int getYaxisOffset();
-		int getZaxisOffset();
-		int getTapDuration();
-		int getTapLatency();
-		int getTapWindow();
-		int getActivityThreshold();
-		int getInactivityThreshold();
-		int getInactivityTime();
-		int getActInactControl();
-		int getFreeFallThresh();
-		int getFreeFallTime();
-		int getTapAxes();
-		int getTapStatus();
-		int getBitrate();
-		int getPowerControl();
-		int getInterruptEnable();
-		int getInterruptMap();
-		int getInterruptSource();
-		int getDataFormat();
-		int getXAccel();
-		int getYAccel();
-		int getZAccel();
-		int getFIFOControl();
-		int getFIFOStatus();
+		unsigned char getDevId();
+		unsigned char getTapThreshold();
+		char getXaxisOffset();
+		char getYaxisOffset();
+		char getZaxisOffset();
+		unsigned char getTapDuration();
+		unsigned char getTapLatency();
+		unsigned char getTapWindow();
+		unsigned char getActivityThreshold();
+		unsigned char getInactivityThreshold();
+		unsigned char getInactivityTime();
+		unsigned char getActInactControl();
+		unsigned char getFreeFallThresh();
+		unsigned char getFreeFallTime();
+		unsigned char getTapAxes();
+		unsigned char getTapStatus();
+		unsigned char getBitrate();
+		unsigned char getPowerControl();
+		unsigned char getInterruptEnable();
+		unsigned char getInterruptMap();
+		unsigned char getInterruptSource();
+		unsigned char getDataFormat();
+		unsigned short int getXAccel();
+		unsigned short int getYAccel();
+		unsigned short int getZAccel();
+		unsigned char getFIFOControl();
+		unsigned char getFIFOStatus();
 
 		//Write Methods
-		void setTapThreshold(int value);
-		void setXaxisOffset(int value);
-		void setYaxisOffset(int value);
-		void setZaxisOffset(int value);
-		void setTapDuration(int value);
-		void setTapLatency(int value);
-		void setTapWindow(int value);
-		void setActivityThreshold(int value);
-		void setInactivityThreshold(int value);
-		void setInactivityTime(int value);
-		void setActInactControl(int value);
-		void setFreeFallThresh(int value);
-		void setFreeFallTime(int value);
-		void setTapAxes(int value);
-		void setBitrate(int value);
-		void setPowerControl(int value);
-		void setInterruptEnable(int value);
-		void setInterruptSource(int value);
-		void setDataFormat(int value);
-		void setFIFOControl(int value);
+		void setTapThreshold(unsigned char value);
+		void setXaxisOffset(char value);
+		void setYaxisOffset(char value);
+		void setZaxisOffset(char value);
+		void setTapDuration(unsigned char value);
+		void setTapLatency(unsigned char value);
+		void setTapWindow(unsigned char value);
+		void setActivityThreshold(unsigned char value);
+		void setInactivityThreshold(unsigned char value);
+		void setInactivityTime(unsigned char value);
+		void setActInactControl(unsigned char value);
+		void setFreeFallThresh(unsigned char value);
+		void setFreeFallTime(unsigned char value);
+		void setTapAxes(unsigned char value);
+		void setBitrate(unsigned char value);
+		void setPowerControl(unsigned char value);
+		void setInterruptEnable(unsigned char value);
+		void setInterruptSource(unsigned char value);
+		void setDataFormat(unsigned char value);
+		void setFIFOControl(unsigned char value);
 
 };
 
@@ -86,145 +91,161 @@ ADXL345::ADXL345(unsigned char channel,bool alt) {
 };
 
 
+unsigned char ADXL345::getByte(char addr) {
+	char buf[2];
+	buf[0] = addr; buf[1] = '\0';
+	sendLine.send(buf,1);
+	recvLine.recv(buf,1);
+	return buf[0];
+}
+
+void ADXL345::sendByte(char addr, char value) {
+	char buf[3];
+	buf[0] = 0x1D; buf[1] = value; buf[2] = '\0';
+	sendLine.send(buf,2);
+}
+
+
 //Read Methods
-int ADXL345::getDevId() {
-	char buf[2];
-	buf[0] = 0x00; buf[1] = '\0';
-	sendLine.send(buf,1);
-	recvLine.recv(buf,1);
-	return atoi(buf);
+unsigned char ADXL345::getDevId() {
+	return getByte(0x00);
 }
 
-int ADXL345::getTapThreshold() {
-	char buf[2];
-	buf[0] = 0x1D; buf[1] = '\0';
-	sendLine.send(buf,1);
-	recvLine.recv(buf,1);
-	return atoi(buf);
+unsigned char ADXL345::getTapThreshold() {
+	return getByte(0x1D);
 }
 
-int ADXL345::getXaxisOffset() {
-	
+char ADXL345::getXaxisOffset() {
+	return (char)getByte(0x1E);
 }
 
 
-int ADXL345::getYaxisOffset() {
-	
+char ADXL345::getYaxisOffset() {
+	return (char)getByte(0x1F);
 }
 
 
-int ADXL345::getZaxisOffset() {
-	
+char ADXL345::getZaxisOffset() {
+	return (char)getByte(0x20);
 }
 
 
-int ADXL345::getTapDuration() {
-	
+unsigned char ADXL345::getTapDuration() {
+	return getByte(0x21);
 }
 
 
-int ADXL345::getTapLatency() {
-	
+unsigned char ADXL345::getTapLatency() {
+	return getByte(0x22);
 }
 
 
-int ADXL345::getTapWindow() {
-	
+unsigned char ADXL345::getTapWindow() {
+	return getByte(0x23);
 }
 
 
-int ADXL345::getActivityThreshold() {
-	
+unsigned char ADXL345::getActivityThreshold() {
+	return getByte(0x24);
 }
 
 
-int ADXL345::getInactivityThreshold() {
-	
+unsigned char ADXL345::getInactivityThreshold() {
+	return getByte(0x25);
 }
 
 
-int ADXL345::getInactivityTime() {
-	
+unsigned char ADXL345::getInactivityTime() {
+	return getByte(0x26);
 }
 
 
-int ADXL345::getActInactControl() {
-	
+unsigned char ADXL345::getActInactControl() {
+	return getByte(0x27);
 }
 
 
-int ADXL345::getFreeFallThresh() {
-	
+unsigned char ADXL345::getFreeFallThresh() {
+	return getByte(0x28);
 }
 
 
-int ADXL345::getFreeFallTime() {
-	
+unsigned char ADXL345::getFreeFallTime() {
+	return getByte(0x29);
 }
 
 
-int ADXL345::getTapAxes() {
-	
+unsigned char ADXL345::getTapAxes() {
+	return getByte(0x2A);
 }
 
 
-int ADXL345::getTapStatus() {
-	
+unsigned char ADXL345::getTapStatus() {
+	return getByte(0x2B);
 }
 
 
-int ADXL345::getBitrate() {
-	
+unsigned char ADXL345::getBitrate() {
+	return getByte(0x2C);
 }
 
 
-int ADXL345::getPowerControl() {
-	
+unsigned char ADXL345::getPowerControl() {
+	return getByte(0x2D);
 }
 
 
-int ADXL345::getInterruptEnable() {
-	
+unsigned char ADXL345::getInterruptEnable() {
+	return getByte(0x2E);
 }
 
 
-int ADXL345::getInterruptMap() {
-	
+unsigned char ADXL345::getInterruptMap() {
+	return getByte(0x2F);
 }
 
 
-int ADXL345::getInterruptSource() {
-	
+unsigned char ADXL345::getInterruptSource() {
+	return getByte(0x30);
 }
 
 
-int ADXL345::getDataFormat() {
-	
+unsigned char ADXL345::getDataFormat() {
+	return getByte(0x31);
 }
 
 
-int ADXL345::getXAccel() {
-	
+unsigned short int ADXL345::getXAccel() {
+	unsigned char lsb = getByte(0x32);
+	unsigned char msb = getByte(0x33);
+
+	return (((unsigned short int)msb) << 8) + lsb;
 }
 
 
-int ADXL345::getYAccel() {
-	
+unsigned short int ADXL345::getYAccel() {
+	unsigned char lsb = getByte(0x34);
+	unsigned char msb = getByte(0x35);
+
+	return (((unsigned short int)msb) << 8) + lsb;
 }
 
 
-int ADXL345::getZAccel() {
-	
+unsigned short int ADXL345::getZAccel() {
+	unsigned char lsb = getByte(0x36);
+	unsigned char msb = getByte(0x37);
+
+	return (((unsigned short int)msb) << 8) + lsb;
 }
 
 
-int ADXL345::getFIFOControl() {
-	
+unsigned char ADXL345::getFIFOControl() {
+	return getByte(0x38);
 }
 
 
-int ADXL345::getFIFOStatus() {
-	
+unsigned char ADXL345::getFIFOStatus() {
+	return getByte(0x39);
 }
 
 
@@ -232,106 +253,104 @@ int ADXL345::getFIFOStatus() {
 //Write Methods
 
 //Scale Factor = 62.5mg/LSB
-//0-255 input
-void ADXL345::setTapThreshold(int value) {
-	char buf[3];
-	buf[0] = 0x1D; buf[1] = (char) LIMIT(0,value,255);
-	sendLine.send(buf,2);
+void ADXL345::setTapThreshold(unsigned char value) {
+	sendByte(0x1D,value);
+}
+
+//Scale Factor = 15.6mg/LSB
+void ADXL345::setXaxisOffset(char value) {
+	sendByte(0x1E,value);
 }
 
 
-void ADXL345::setXaxisOffset(int value) {
+void ADXL345::setYaxisOffset(char value) {
+	sendByte(0x1F,value);
+}
+
+
+void ADXL345::setZaxisOffset(char value) {
+	sendByte(0x20,value);
+}
+
+
+void ADXL345::setTapDuration(unsigned char value) {
+	sendByte(0x21,value);
+}
+
+
+void ADXL345::setTapLatency(unsigned char value) {
+	sendByte(0x22,value);
+}
+
+
+void ADXL345::setTapWindow(unsigned char value) {
+	sendByte(0x23,value);
+}
+
+
+void ADXL345::setActivityThreshold(unsigned char value) {
+	sendByte(0x24,value);
+}
+
+
+void ADXL345::setInactivityThreshold(unsigned char value) {
+	sendByte(0x25,value);
+}
+
+
+void ADXL345::setInactivityTime(unsigned char value) {
+	sendByte(0x26,value);
+}
+
+
+void ADXL345::setActInactControl(unsigned char value) {
+	sendByte(0x27,value);
+}
+
+
+void ADXL345::setFreeFallThresh(unsigned char value) {
+	sendByte(0x28,value);
+}
+
+
+void ADXL345::setFreeFallTime(unsigned char value) {
+	sendByte(0x29,value);
+}
+
+
+void ADXL345::setTapAxes(unsigned char value) {
+	sendByte(0x2A,value);
 	
 }
 
 
-void ADXL345::setYaxisOffset(int value) {
-	
+void ADXL345::setBitrate(unsigned char value) {
+	sendByte(0x2C,value);
 }
 
 
-void ADXL345::setZaxisOffset(int value) {
-	
+void ADXL345::setPowerControl(unsigned char value) {
+	sendByte(0x2D,value);
 }
 
 
-void ADXL345::setTapDuration(int value) {
-	
+void ADXL345::setInterruptEnable(unsigned char value) {
+	sendByte(0x2E,value);
 }
 
 
-void ADXL345::setTapLatency(int value) {
-	
+void ADXL345::setInterruptSource(unsigned char value) {
+	sendByte(0x2F,value);
 }
 
 
-void ADXL345::setTapWindow(int value) {
-	
+void ADXL345::setDataFormat(unsigned char value) {
+	sendByte(0x31,value);
 }
 
 
-void ADXL345::setActivityThreshold(int value) {
-	
-}
-
-
-void ADXL345::setInactivityThreshold(int value) {
-	
-}
-
-
-void ADXL345::setInactivityTime(int value) {
-	
-}
-
-
-void ADXL345::setActInactControl(int value) {
-	
-}
-
-
-void ADXL345::setFreeFallThresh(int value) {
-	
-}
-
-
-void ADXL345::setFreeFallTime(int value) {
-	
-}
-
-
-void ADXL345::setTapAxes(int value) {
-	
-}
-
-
-void ADXL345::setBitrate(int value) {
-	
-}
-
-
-void ADXL345::setPowerControl(int value) {
-	
-}
-
-
-void ADXL345::setInterruptEnable(int value) {
-	
-}
-
-
-void ADXL345::setInterruptSource(int value) {
-	
-}
-
-
-void ADXL345::setDataFormat(int value) {
-	
-}
-
-
-void ADXL345::setFIFOControl(int value) {
-	
+void ADXL345::setFIFOControl(unsigned char value) {
+	sendByte(0x38,value);
 }
 
 
