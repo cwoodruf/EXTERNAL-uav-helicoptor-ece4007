ecd2129e25aca76ddef8b1690441c648f435e90e
joey <joey@jyore.com>
2012-01-31 13:46:37 -0500

    Updated Timeout to work with multiple timeouts

--- software/time/timeout.h
+++ software/time/timeout.h
@@ -1,86 +1,160 @@
 /*
-Copyright (c) 2012 Joey Yore
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* Copyright (c) 2012 Joey Yore
+*
+* Permission is hereby granted, free of charge, to any person obtaining a 
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation 
+* the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the 
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included 
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+* OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#ifndef TIMEOUT_H
+#define TIMEOUT_H
+
+
+#include <iostream>
 #include <signal.h>
-#include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
+#include "../datastructures/list.h"
 
-class Timeout {
+using namespace std;
 
-	private:
-		struct sigaction sa;
-		struct itimerval timer;
+namespace timeout {
 
-	public:
-		Timeout();
-		Timeout(void (*fptr)(int), double t);
-		Timeout(void (*fptr)(int), unsigned int t);
+	struct TimeoutNode {
+		void (*cb)();
+		double diff;
+	};
 
-		void setup(void (*fptr)(int),double t);
-		void setup(void (*fptr)(int),unsigned int t);
-		void start();
-		void stop();
-		void clear();
-};
+	List<TimeoutNode> toList;
+	struct itimerval itimer;
+	struct sigaction saction;
+	double currentTime;
 
-Timeout::Timeout() {
+	void dispatcher(int sig) {
+		TimeoutNode to;
+		toList.pop_front(to);
+		to.cb();
+			
+		if(!toList.isEmpty()) {
+			ListIterator<TimeoutNode> it=toList.begin();
 
+			while((*it).diff == 0) {
+				toList.pop_front(to);
+				to.cb();
+				it=toList.begin();
+			}
+		
+			currentTime = (*it).diff;
+
+			for(;it!=toList.end();++it) {
+				(*it).diff = (*it).diff - currentTime;
+			}
+			(*it).diff = (*it).diff - currentTime;
+
+			unsigned int s = (unsigned int)currentTime;
+			unsigned int us = (unsigned int)((currentTime-s)*1000000);
+			itimer.it_value.tv_sec = s;
+			itimer.it_value.tv_usec = us;
+			itimer.it_interval.tv_sec = 0;
+			itimer.it_interval.tv_usec = 0;
+			setitimer(ITIMER_VIRTUAL,&timeout::itimer,0);
+
+		}
+	}
 }
 
-Timeout::Timeout(void (*fptr)(int),double t) {
-	setup(fptr,t);
+
+int register_timeout(void (*func)(),double time) {
+	static bool first_run = true;
+
+	if(first_run) {
+		memset(&timeout::saction,0,sizeof(timeout::saction));
+		timeout::saction.sa_handler = timeout::dispatcher;
+		sigaction(SIGVTALRM,&timeout::saction,0);
+		first_run = false;
+	}
+
+
+	timeout::TimeoutNode to;
+
+	if(timeout::toList.isEmpty()) {
+		to.cb = func;
+		to.diff = 0;
+		timeout::toList.push(to);
+		timeout::currentTime = time;
+
+		unsigned int s = (unsigned int)time;
+		unsigned int us = (unsigned int)((time-s)*1000000);
+		timeout::itimer.it_value.tv_sec = s;
+		timeout::itimer.it_value.tv_usec = us;
+		timeout::itimer.it_interval.tv_sec = 0;
+		timeout::itimer.it_interval.tv_usec = 0;
+
+		setitimer(ITIMER_VIRTUAL,&timeout::itimer,0);
+		return 0;
+	}
+
+	ListIterator<timeout::TimeoutNode> it=timeout::toList.begin();
+	
+	//is front?
+	if(time < ((*it).diff+timeout::currentTime)) {
+		for(;it!=timeout::toList.end();++it) {
+			(*it).diff = (*it).diff + timeout::currentTime - time;
+		}
+		(*it).diff = (*it).diff + timeout::currentTime - time;
+
+		to.diff = 0;
+		to.cb = func;
+		timeout::toList.push_front(to);
+		timeout::currentTime = time;
+
+		unsigned int s = (unsigned int)time;
+		unsigned int us = (unsigned int)((time-s)*1000000);
+		timeout::itimer.it_value.tv_sec = s;
+		timeout::itimer.it_value.tv_usec = us;
+		timeout::itimer.it_interval.tv_sec = 0;
+		timeout::itimer.it_interval.tv_usec = 0;
+
+		setitimer(ITIMER_VIRTUAL,&timeout::itimer,0);
+
+	} else {
+		int place = 0;
+		bool found = false;
+		to.diff = time - timeout::currentTime;
+		to.cb = func;
+
+		for(;it!=timeout::toList.end();++it) {
+			if(time <= (*it).diff + timeout::currentTime) {
+				found = true;
+				break;
+			}
+			++place;
+		}
+		if(!found) {
+			if(time > (*it).diff + timeout::currentTime) {
+				timeout::toList.push(to);	
+			} else {
+				timeout::toList.insert(to,place);
+			}
+		} else {
+			timeout::toList.insert(to,place);
+		}
+	}
+
+	return 0;
 }
-
-Timeout::Timeout(void (*fptr)(int), unsigned int t) {
-	setup(fptr,t);
-}
-
-void Timeout::setup(void (*fptr)(int),double t) {
-
-	unsigned int sec = (unsigned int)t;
-	unsigned int usec = (unsigned int)((t-sec)*1000000);
-
-	memset(&sa, 0, sizeof(sa));
-	sa.sa_handler = fptr;
-	sigaction(SIGVTALRM, &sa, NULL);
-
-
-	timer.it_value.tv_sec = sec;
-	timer.it_value.tv_usec = usec;
-	timer.it_interval.tv_sec = 0;
-	timer.it_interval.tv_usec = 0;
-
-}
-
-void Timeout::setup(void (*fptr)(int), unsigned int t) {
-	memset(&sa, 0, sizeof(sa));
-	sa.sa_handler = fptr;
-	sigaction(SIGVTALRM, &sa, NULL);
-
-
-	timer.it_value.tv_sec = 0;
-	timer.it_value.tv_usec = t;
-	timer.it_interval.tv_sec = 0;
-	timer.it_interval.tv_usec = 0;
-
-}
-
-void Timeout::start() {
-	setitimer(ITIMER_VIRTUAL, &timer, NULL);
-}
-
-void Timeout::clear() {
-	timer.it_value.tv_sec = 0;
-	timer.it_value.tv_usec = 0;
-	timer.it_interval.tv_sec = 0;
-	timer.it_interval.tv_usec = 0;
-	setitimer(ITIMER_VIRTUAL, &timer, NULL);
-}
+#endif
